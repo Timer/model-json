25f22b0bd042661de6945400c7dd5c9102e743cc
Joe Haddad <timer150@gmail.com>
2016-06-30 21:40:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# model-json
+Enforces a schema on a JSON object.
+
+### Install
+```
+$ npm i model-json -S
+```
+
+### Usage
+```js
+// TODO: example usage
+```
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import _ from 'lodash'
+
+const TYPES = ['array', 'boolean', 'function', 'integer', 'number', 'object', 'string']
+const PARSERS = {
+  array: v => Array.isArray(v) ? v : undefined,
+  boolean: v => (/^true|t|yes|y|1$/i.test(v) || v === true),
+  function: v => typeof v === 'function' ? v : undefined,
+  integer: v => {
+    v = parseInt(v)
+    if (isNaN(v)) return undefined
+    return v
+  },
+  number: v => {
+    v = parseFloat(v)
+    if (isNaN(v)) return undefined
+    return v
+  },
+  object: v => typeof v === 'object' ? v : undefined,
+  string: v => v == null ? undefined : v.toString()
+}
+
+export default class ModelJson {
+  constructor(schema) {
+    this.schema = schema
+
+    for (let key in schema) {
+      let m = schema[key]
+      let { type } = m
+      if (TYPES.indexOf(type) === -1) throw new Error('Type must be one of ' + TYPES)
+    }
+  }
+
+  validate(object, options = { }) {
+    let { schema } = this
+
+    let keys = _.keys(schema)
+    object = _.pick(_.clone(object), keys)
+    for (let key of keys) {
+      if (!object.hasOwnProperty(key)) continue
+      let m = schema[key], parse = PARSERS[m.type]
+      let ot = typeof object[key]
+
+      object[key] = parse(object[key])
+
+      if (object[key] === undefined) {
+        if (!m.required) delete object[key]
+        else if (m.default !== undefined) object[key] = m.default
+        else throw new Error(`Key '${key}' is of invalid type, found: '${ot}', required: '${m.type}'.`)
+      }
+    }
+    return object
+  }
+}
